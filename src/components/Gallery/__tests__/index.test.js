c455e00d39b8d5dee908e2bf8f56e84dac0e5c6e
Tez28 <courtezcannady@gmail.com>
2022-05-13 12:15:04 -0400

    contact form

--- src/components/Gallery/__tests__/index.test.js
+++ src/components/Gallery/__tests__/index.test.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { render, cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import Gallery from '..'
+
 const portrait = { name: "portraits", description: "Portraits of people in my life" };
 
 afterEach(cleanup)
